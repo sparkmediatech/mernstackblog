a1ea70b7519153e634c4ab4bf80efc88fa59fca9
unknown <conventionalanimation@gmail.com>
2022-10-26 23:30:52 +0100

    worked on mongoose auto conenction

--- api/db/connect.js
+++ api/db/connect.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose')
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
+    keepAlive: true,
+    autoReconnect: true,
     useCreateIndex: true,
+    reconnectTries:Number.MAX_VALUE, 
+    reconnectInterval: 5000,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
