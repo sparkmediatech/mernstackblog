0f4f093dd5db26443489e5eccad26dbad1439139
unknown <conventionalanimation@gmail.com>
2022-10-26 16:19:03 +0100

    updated api url for client for production

--- client/src/hooks/Base_URL.js
+++ client/src/hooks/Base_URL.js
@@ -1,6 +1,6 @@
-//const BASE_URL = "/api/v1";
+const BASE_URL = "/api/v1";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+//const BASE_URL = "http://localhost:5000/api/v1";
 
 
 export default BASE_URL
