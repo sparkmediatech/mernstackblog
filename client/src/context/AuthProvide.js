471889ce25ca727237b02d1e7ec77dfe37b62621
unknown <conventionalanimation@gmail.com>
2022-10-27 16:09:48 +0100

    corrected refresh token api link

--- client/src/context/AuthProvide.js
+++ client/src/context/AuthProvide.js
@@ -83,7 +83,7 @@ export const AuthProvider = ({children})=>{
     const refreshToken = async () =>{
         
         try{
-                const response = await axios.post(`/refresh`, {
+                const response = await axios.post(`${BASE_URL}/refresh`, {
                  withCredentials: true
              });
              setAuth(response.data);
