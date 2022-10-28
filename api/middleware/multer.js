fddd663453485a22a4be4e6d415928b7ee2028bc
unknown <conventionalanimation@gmail.com>
2022-10-28 06:57:15 +0100

    fixed upload image error by removing destination object in multer

--- api/middleware/multer.js
+++ api/middleware/multer.js
@@ -7,16 +7,16 @@ const multer = require("multer");
         const ALLOWED_FORMATS = ['image/jpeg', 'image/png', 'image/jpg'];
 
         const storage = multer.diskStorage({//choosing destination of the file
-        destination:(req, file, cb) =>{
-        if(ALLOWED_FORMATS.includes(file.mimetype)){
-             //cb(null, "./images")
-              cb(null, true)
-        }else{
-            cb(new Error('Not supported file type!'), false)
-        }
-       
-    }, filename:(req, file,cb)=> {//choosing file name
+         filename:(req, file,cb)=> {//choosing file name
        cb(null, req.body.name) 
+    },
+    fileFilter: (req, file, cd)=>{
+        if(ALLOWED_FORMATS.includes(file.mimetype)){
+            //cb(null, "./images")
+             cb(null, true)
+       }else{
+           cb(new Error('Not supported file type!'), false)
+       }
     }
 });
 
