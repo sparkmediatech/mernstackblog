c0c14d883f027c0fbba88646ace0948b3a9b59bc
unknown <conventionalanimation@gmail.com>
2022-10-28 22:38:10 +0100

    fixed the bug in single post conponent which was causing deployment to fail

--- client/src/pages/single/single.jsx
+++ client/src/pages/single/single.jsx
@@ -1,9 +1,10 @@
+import React, {useEffect, useState, useContext} from 'react';
 import './single.css'
 import Sidebar from '../../components/sidebar/sidebar'
 import SinglePost from '../../components/singlePost/singlePost'
 import {AuthContext} from '../../context/AuthProvide';
 
-export default function single() {
+export default function Single() {
     const {postLength} = useContext(AuthContext);
     //global post length that allows sidebar to be shown only whenthe length of post component is greater than 0 and improve user experience
 
