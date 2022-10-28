862fd9152dbc9be30f7fcce20fb9acbe3f4dd756
unknown <conventionalanimation@gmail.com>
2022-10-28 05:28:29 +0100

    updated multer function

--- api/middleware/multer.js
+++ api/middleware/multer.js
@@ -8,12 +8,7 @@ const multer = require("multer");
 
         //login to upload images using libery called multer
         const storage = multer.diskStorage({//choosing destination of the file
-        destination:(req, file, cb) =>{
-        if(!ALLOWED_FORMATS.includes(file.mimetype)){
-            cb(new Error('Not supported file type!'), false)
-             //cb(null, "./images")
-        }
-    }, filename:(req, file,cb)=> {//choosing file name
+        filename:(req, file,cb)=> {//choosing file name
        cb(null, req.body.name) 
     }
 });
