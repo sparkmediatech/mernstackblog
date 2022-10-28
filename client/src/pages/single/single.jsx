7769d07da4c7d0615d19d26a9428508a3a4e1d47
unknown <conventionalanimation@gmail.com>
2022-10-28 19:00:36 +0100

    updated

--- client/src/pages/single/single.jsx
+++ client/src/pages/single/single.jsx
@@ -5,7 +5,7 @@ import {AuthContext} from '../../context/AuthProvide';
 
 export default function single() {
     const {postLength} = useContext(AuthContext);
-    //global post length that allows sidebar to be shown only whenthe length of post component is greater than 0
+    //global post length that allows sidebar to be shown only whenthe length of post component is greater than 0 and improve user experience
 
 
     return (
