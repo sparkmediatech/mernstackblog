047e92759722c41385215d0b69eca1b80e3453cc
sparkmediatech <conventionalanimation@gmail.com>
2022-11-07 10:45:23 +0100

    trying redployment

--- api/db/connect.js
+++ api/db/connect.js
@@ -8,6 +8,8 @@ const MONGO_URL = process.env.MONGO_URL
 const connectDB = async(url) => {
     mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
+    keepAlive: true,
+    autoReconnect: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
